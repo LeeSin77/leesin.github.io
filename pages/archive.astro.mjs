510269fa7d3f7e590ce78a24c3797c6c7d6cfd54
LeeSin77 <slee82764@gmail.com>
2025-12-16 23:06:33 +0800

    Delete pages directory

--- pages/archive.astro.mjs
+++
@@ -1,6 +0,0 @@
-import { _ as _page } from '../chunks/archive_DpvBnGD_.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_DkFg_9vY.mjs';
-
-const page = () => _page;
-
-export { page };
